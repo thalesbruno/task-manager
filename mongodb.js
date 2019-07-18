b8238f82785238c3c883b89045aea7888bfcc19b
Thales Bruno <thalesbrunom@gmail.com>
2019-07-18 05:28:06 -0300

    install and use

--- mongodb.js
+++ mongodb.js
@@ -6,12 +6,6 @@ const { MongoClient, ObjectID } = mongodb
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-
-const id = new ObjectID()
-
-console.log(id)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to database')
@@ -20,48 +14,9 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   // no need to create a database, just use it
   const db = client.db(databaseName)
 
-  // db.collection('users').insertOne({
-  //   name: 'Thales',
-  //   age: 29
-  // }, (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to insert document')
-  //   }
+  db.collection('tasks').deleteOne({ description: "translate node.js documentation" })
+    .then((result) => console.log(result.deletedCount))
+    .catch((error) => console.error(error))
 
-  //   console.log(result.ops)
-  // })
-
-  // db.collection('users').insertMany([
-  //   {
-  //     name: 'Nietzsche',
-  //     age: 45
-  //   }, {
-  //     name: 'Heigel',
-  //     age: 57
-  //   }
-  // ], (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to create documents')
-  //   }
-
-  //   console.log(result.ops)
-  // })
-
-  // db.collection('tasks').insertMany([{
-  //   description: 'buy a monitor',
-  //   completed: false
-  // }, {
-  //   description: 'watch lion king',
-  //   completed: false
-  // }, {
-  //   description: 'translate node.js documentarion',
-  //   completed: true
-  // }
-  // ], (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to create collection')
-  //   }
-
-  //   console.log(result.ops)
-  // })
+  client.close()
 })
