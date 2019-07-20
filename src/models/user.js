0aa0366d94233105ce03607d6777f83b09dd68ff
Thales Bruno <thalesbrunom@gmail.com>
2019-07-20 10:00:20 -0300

    add create endpoints

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) throw new Error('e-mail is invalid')
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+    validate(value) {
+      if (value.includes("password")) throw new Error("Password cannot contain 'password' string")
+    }
+  },
+  age: {
+    type: Number,
+    min: 0
+  }
+})
+
+module.exports = User
