81490c03bd968906037792d08a677abb9d1d3f46
Thales Bruno <thalesbrunom@gmail.com>
2019-07-21 21:27:51 -0300

    refactor to async-await

--- src/index.js
+++ src/index.js
@@ -8,18 +8,68 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
-  user.save()
-    .then(() => res.status(201).send(user))
-    .catch((error) => res.status(400).send(error))
+
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch(error) {
+    res.status(400).send(error)
+  }
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
-  task.save()
-    .then(() => res.status(201).send(task))
-    .catch((error) => res.status(400).send(error))
+
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch(error) {
+    res.status(400).send(error)
+  }
+})
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.send(users)
+  } catch(error) {
+    res.send(error)
+  }
+})
+
+app.get('/users/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const user = await User.findById(_id)
+    if (!user) return res.status(404).send()
+    res.send(user)
+  } catch(error) {
+    res.status(500).send(error)
+  }
+})
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    res.send(tasks)
+  } catch(error) {
+    res.status(500).send(error)
+  }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const task = await Task.findById(_id)
+    if (!task) return res.status(404).send({message: 'No task with this ID'})
+    res.send(task)
+  } catch(error) {
+    res.status(500).send(error)
+  }
 })
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
