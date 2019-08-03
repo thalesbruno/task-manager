b8ed57fe7247791f0fe8d98c01f2d0c81424f728
Thales Bruno <thalesbrunom@gmail.com>
2019-08-03 17:42:42 -0300

    add mongodb_dbname environment variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/task-manager-api`, {
+mongoose.connect(`mongodb://${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DBNAME}`, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
