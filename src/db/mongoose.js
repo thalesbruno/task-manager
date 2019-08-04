72cb7e200215fee708a4eab448cd3c9adc13c848
Thales Bruno <thalesbrunom@gmail.com>
2019-08-04 17:54:43 -0300

    add dockerfile and docker-compose files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DBNAME}`, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(`mongodb://${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DBNAME}`, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+  } catch(error) {
+    console.error(error)
+  }
+}
+
+dbConnect()
--- src/index.js
+++ src/index.js
@@ -7,48 +7,12 @@ const taskRoute = require('./routes/task')
 const app = express()
 const port = process.env.PORT
 
-// app.use((req, res, next) => {
-//   res.status(503).send('Service temporarily unavailable')
-// })
-
-const multer = require('multer')
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, callback) {
-    if (!file.originalname.match(/\.(pdf|mobi)$/)) {
-      callback(new Error('We only accept PDF files!'))
-    }
-    callback(undefined, true)
-  }
+app.get('/', (req, res) => {
+  res.send('Server running!')
 })
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-})
-
 
 app.use(express.json())
 app.use(userRoute)
 app.use(taskRoute)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async () => {
-  // const task = await Task.findById('5d3e20a695289f3ac9447933')
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner)
-
-//   const user = await User.findById('5d3e1f4f153cc63aa6047a21')
-//   await user.populate('tasks').execPopulate()
-//   console.log(user.tasks)
-// }
-
-// main()
